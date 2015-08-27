bf56d020ae46bd24a3fd29d20ab57015d04cbab0
Marc-Antoine Tremblay <ma.an.tr@gmail.com>
2015-08-27 14:31:44 -0400

    try html geolocation

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 ﻿
 $(document).ready(function(){
   
-  console.log('test');
-  
+  var x = $('textarea[name*=textarea_info]');
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+
+}
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude; 
+}
+
 });
