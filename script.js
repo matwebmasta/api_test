1c82eb1161679d0f67196e0b28e403d4aa0a39ef
Marc-Antoine Tremblay <ma.an.tr@gmail.com>
2015-08-27 14:45:14 -0400

    Place button and exception for geolocation

--- script.js
+++ script.js
@@ -3,15 +3,42 @@ $(document).ready(function(){
   
   var x = $('textarea[name*=textarea_info]');
 
+  $("button.startGeo").on('touchstart mousedown',function(){
+    startGeo();
+    
+  });
+  
+  function startGeo(){
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else { 
+        x.val("Geolocation is not supported by this browser.");
 
-}
+    }
+    
+  }
 function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude + 
-    "<br>Longitude: " + position.coords.longitude; 
+    x.val("Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude);
 }
+
+function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            x.val("User denied the request for Geolocation.");
+            break;
+        case error.POSITION_UNAVAILABLE:
+            x.val("Location information is unavailable.");
+            break;
+        case error.TIMEOUT:
+            x.val("The request to get user location timed out.");
+            break;
+        case error.UNKNOWN_ERROR:
+            x.val("An unknown error occurred.");
+            break;
+    }
+}
+
+
 
 });
