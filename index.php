b382b5a6e2111dc4519a54fad032164f4bd82ff4
Marc-Antoine Tremblay <ma.an.tr@gmail.com>
2015-09-10 23:48:05 -0400

    Start to connect apis

---
+++ index.php
@@ -0,0 +1,91 @@
+﻿<?php
+sessionStart();
+define("CLIENT_IP",$_SERVER['REMOTE_ADDR']);
+?>
+
+<html>
+<head>
+<script src="http://code.jquery.com/jquery-latest.js" ></script>
+<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=false"></script> 
+<script src="./script.js" ></script>
+
+</head>
+<?php
+
+$client_city = detect_city(CLIENT_IP);
+
+function detect_city($ip) {
+        
+        $default = 'UNKNOWN';
+
+        if (!is_string($ip) || strlen($ip) < 1 || $ip == '127.0.0.1' || $ip == 'localhost')
+            $ip = '8.8.8.8';
+
+        $curlopt_useragent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.2) Gecko/20100115 Firefox/3.6 (.NET CLR 3.5.30729)';
+        
+        $url = 'http://ipinfodb.com/ip_locator.php?ip=' . urlencode($ip);
+        $ch = curl_init();
+        
+        $curl_opt = array(
+            CURLOPT_FOLLOWLOCATION  => 1,
+            CURLOPT_HEADER      => 0,
+            CURLOPT_RETURNTRANSFER  => 1,
+            CURLOPT_USERAGENT   => $curlopt_useragent,
+            CURLOPT_URL       => $url,
+            CURLOPT_TIMEOUT         => 1,
+            CURLOPT_REFERER         => 'http://' . $_SERVER['HTTP_HOST'],
+        );
+        
+        curl_setopt_array($ch, $curl_opt);
+        
+        $content = curl_exec($ch);
+        
+        if (!is_null($curl_info)) {
+            $curl_info = curl_getinfo($ch);
+        }
+        
+        curl_close($ch);
+        
+        if ( preg_match('{<li>City : ([^<]*)</li>}i', $content, $regs) )  {
+            $city = $regs[1];
+        }
+        /*if ( preg_match('{<li>State/Province : ([^<]*)</li>}i', $content, $regs) )  {
+            $state = $regs[1];
+        }*/
+
+        if( $city!=''){
+          $location = $city;
+          return $location;
+        }else{
+          return $default; 
+        }
+        
+}
+
+
+?>
+<body>
+
+<div class="header">
+<h1> Hi human, <br/>Your ip is <?php echo CLIENT_IP; ?></h1>
+<h2>Based on this information, your city is approximately <?php echo $client_city; ?></h2>
+<h2>By comparison, the html 5 way to detect your location returned <span class="js_city"></span>.</h2>
+<h2>From this information, we know that the current weather at your location is <span class="js_weather"></span></h2>
+
+
+</div>
+
+<div class="main_content">
+<button class="startGeo" value="" >Get started!</button>
+<textarea name="textarea_info" rows="10" cols="50">Now loading.</textarea>
+
+<input name="try" type='text' value='enter something here..' />
+
+
+</div>
+
+
+
+
+</body>
+</html>
--- script.js
+++ script.js
@@ -1,59 +1,67 @@
 ﻿
+  var geocoder;
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
+} 
+//Get the latitude and the longitude;
+function successFunction(position) {
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
+    codeLatLng(lat, lng)
+}
+
+function errorFunction(){
+    alert("Geocoder failed");
+}
+
+  function initialize() {
+    geocoder = new google.maps.Geocoder();
+
+
+
+  }
+
+  function codeLatLng(lat, lng) {
+
+    var latlng = new google.maps.LatLng(lat, lng);
+    geocoder.geocode({'latLng': latlng}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+      console.log(results)
+        if (results[1]) {
+         //formatted address
+         alert(results[0].formatted_address)
+        //find country name
+             for (var i=0; i<results[0].address_components.length; i++) {
+            for (var b=0;b<results[0].address_components[i].types.length;b++) {
+
+            //there are different types that might hold a city admin_area_lvl_1 usually does in come cases looking for sublocality type will be more appropriate
+                if (results[0].address_components[i].types[b] == "administrative_area_level_1") {
+                    //this is the object you are looking for
+                    city= results[0].address_components[i];
+                    break;
+                }
+            }
+        }
+        //city data
+        $('.js_city').html(city.short_name + " " + city.long_name);
+
+
+        } else {
+          alert("No results found");
+        }
+      } else {
+        alert("Geocoder failed due to: " + status);
+      }
+    });
+  }
+
+
+
+
+
 $(document).ready(function(){
   
-  var x = $('textarea[name*=textarea_info]');
-
-  $("button.startGeo").on('touchstart mousedown',function(){
-    startGeo();
-    
-  });
-  
-  function startGeo(){
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition, showError);
-    } else { 
-        x.val("Geolocation is not supported by this browser.");
-
-    }
-    
-  }
-function showPosition(position) {
-    x.val("NOW LOADING! Debug datas follows\nLatitude: " + position.coords.latitude + 
-    "\nLongitude: " + position.coords.longitude);
-    
-    var request = $.ajax({
-      url:"http://api.openweathermap.org/data/2.5/weather?lat="+position.coords.latitude+"&lon="+position.coords.longitude,
-      dataType: "json",
-    });
-    
-    request.done(function( msg ) {
-      var result = msg;
-      x.val(result);
-    });
-    request.fail(function( jqXHR, textStatus ) {
-      x.val("Request failed: " + textStatus );
-    });
-    
-    
-    
-}
-
-function showError(error) {
-    switch(error.code) {
-        case error.PERMISSION_DENIED:
-            x.val("User denied the request for Geolocation.");
-            break;
-        case error.POSITION_UNAVAILABLE:
-            x.val("Location information is unavailable.");
-            break;
-        case error.TIMEOUT:
-            x.val("The request to get user location timed out.");
-            break;
-        case error.UNKNOWN_ERROR:
-            x.val("An unknown error occurred.");
-            break;
-    }
-}
 
 
 
